acb44717adc5e30aa08eb85f4dc2d215a735f1e5
Ronaldo Rodríguez Bermúdez <r2.bermudez@gmail.com>
2023-11-20 20:09:26 -0600

    Update manufacturerFindAll.js

--- netlify/functions/manufacturerFindAll.js
+++ netlify/functions/manufacturerFindAll.js
@@ -16,9 +16,10 @@ exports.handler = async (event, context) => {
     const keys = (await redis.keys('manufacturer_*')).filter(id => id !== 'manufacturer_N');
     const manufacturers = await redis.mget(keys);
 
-    manufacturers.forEach(JSON.parse);
 
-    return { statusCode: 200, headers, body: JSON.stringify(manufacturers) };
+    const parsedmanufacturers = manufacturers.map(manufacturer => JSON.parse(manufacturer));
+
+    return { statusCode: 200, headers, body: JSON.stringify(parsedmanufacturers) };
   } catch (error) {
     console.log(error);
     return { statusCode: 400, headers, body: JSON.stringify(error) };
